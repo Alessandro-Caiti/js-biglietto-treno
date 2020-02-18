ae13b0d7e7b2520803c030d46f0c261c42a2d29f
Alessandro Caiti <acaiti@hotmail.it>
2020-02-18 16:22:48 +0100

    Sembra funzionare

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// quanti chilometri?
+// età del passeggero?
+// 0.21€ al chilometro prezzo biglietto
+// sconto 20% minorenni
+// sconto 40% over 65
+// risultato quanto costa il biglietto?
+
+var chilometri = prompt("Quanti chilometri vuole percorrere?");
+var età = prompt("Quanti anni ha?");
+var costoChilometro = 0.21;
+var prezzoStandard = chilometri * costoChilometro;
+// console.log(prezzoStandard);
+var scontoMinorenne = prezzoStandard - ((prezzoStandard / 100) * 20);
+// console.log(scontoMinorenne);
+var scontoMaturo = prezzoStandard - ((prezzoStandard / 100) * 40);
+// console.log(scontoMaturo);
+
+if (età < 18) {
+    console.log("Ecco il prezzo del suo biglietto scontato del 20% " + scontoMinorenne);
+} else if (età >= 65) {
+    console.log("Ecco il prezzo del suo biglietto scontato del 40% " + scontoMaturo);
+} else {
+    console.log("Ecco il prezzo del suo biglietto " + prezzoStandard);
+}
